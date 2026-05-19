34345b5cc84a687a860f9fabe6c6a2d4b0fb4afe
Jimin Lim <2343758@donga.ac.kr>
2026-05-19 17:14:35 +0900

    train and predict

--- wineKNN.py
+++ wineKNN.py
@@ -1,5 +1,7 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
 
 wine = datasets.load_wine()
 
@@ -9,5 +11,9 @@ y=wine.target
 # 8:2 분할
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=4)
 
-print(X_train.shape)
-print(X_test.shape)
+knn = KNeighborsClassifier(n_neighbors=6)
+knn.fit(X_train,y_train)
+
+y_pred=knn.predict(X_test)
+scores = metrics.accuracy_score(y_test,y_pred)
+print(scores)
